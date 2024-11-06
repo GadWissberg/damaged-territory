a9f1240a5ae24821799e4795f527c8ddd43619ea
Gad Wissberg <gadw17@gmail.com>
2024-11-06 21:28:49 +0200

    Fixing bugs.

--- core/src/main/java/com/gadarts/returnfire/GameDebugSettings.kt
+++ core/src/main/java/com/gadarts/returnfire/GameDebugSettings.kt
@@ -7,11 +7,11 @@ object GameDebugSettings {
     const val SHOW_GL_PROFILING = true
     const val DEBUG_INPUT = false
     const val UI_DEBUG = false
-    const val SFX = true
+    const val SFX = false
     const val DISPLAY_PROPELLER = true
     const val HIDE_PLAYER = false
     const val HIDE_FLOOR = false
-    const val FORCE_PLAYER_HP = -1
+    const val FORCE_PLAYER_HP = 100000
     const val AVOID_PARTICLE_EFFECTS_DRAWING = false
     const val ENABLE_PROFILER = true
     const val DISABLE_HUD = false
