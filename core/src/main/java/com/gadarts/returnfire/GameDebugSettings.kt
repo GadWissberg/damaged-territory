c8c4a7e59ba447221244f325bb23d9f44da46ed8
Gad Wissberg <gadw17@gmail.com>
2025-01-11 11:50:48 +0200

    Working on Apache AI.

--- core/src/main/java/com/gadarts/returnfire/GameDebugSettings.kt
+++ core/src/main/java/com/gadarts/returnfire/GameDebugSettings.kt
@@ -14,12 +14,12 @@ object GameDebugSettings {
     const val HIDE_PLAYER = false
     const val HIDE_FLOOR = false
     const val HIDE_ENEMIES = false
-    const val FORCE_PLAYER_HP = 1000000
+    const val FORCE_PLAYER_HP = -1
     const val AVOID_PARTICLE_EFFECTS_DRAWING = false
     const val ENABLE_PROFILER = true
     const val DISABLE_HUD = false
     val SELECTED_VEHICLE = SimpleCharacterDefinition.APACHE
-    const val FORCE_AIM = -1
+    const val FORCE_AIM = 1
     const val AI_DISABLED = false
-    const val AI_ATTACK_DISABLED = true
+    const val AI_ATTACK_DISABLED = false
 }
