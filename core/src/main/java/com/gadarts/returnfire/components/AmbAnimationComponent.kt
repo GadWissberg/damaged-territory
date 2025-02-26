c7c188c1e5efb1b5fc28a41bed83cdd6c22d04a9
Gad Wissberg <gadw17@gmail.com>
2025-02-26 09:03:56 +0200

    Working on trees.

--- core/src/main/java/com/gadarts/returnfire/components/AmbAnimationComponent.kt
+++ core/src/main/java/com/gadarts/returnfire/components/AmbAnimationComponent.kt
@@ -13,13 +13,12 @@ class AmbAnimationComponent(private val modelInstance: ModelInstance) : Componen
     val animationController: AnimationController = AnimationController(modelInstance)
 
     init {
-        play()
         animationController.allowSameAnimation = true
     }
 
     fun play() {
-        animationController.setAnimation(modelInstance.animations[0].id, 0)
-        nextPlay = TimeUtils.millis() + MathUtils.random(5) * 1000L
+        animationController.setAnimation(modelInstance.animations[0].id, 1)
+        nextPlay = TimeUtils.millis() + MathUtils.random(25) * 1000L
     }
 
 }
